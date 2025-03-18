e1d0a9c535fead3da1bd2f66dc6ed716e6ee8aa4
GhazanfaAli <salam032136muet@gmail.com>
2025-03-18 21:39:33 +0530

    h

--- djangobackend2/djangobackend2/settings.py
+++ djangobackend2/djangobackend2/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = 'django-insecure-f7t(7@e-4o*=sc1x1%r6-u+c^gpxbm1=)%9fs2&l+(u3o8uf8-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', "django-auth-api-22-11b0cjuis-ghazanfar-alis-projects-dcaacf05.vercel.app",
-]
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
